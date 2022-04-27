3f9c846a685d8cfee69ac44fc1fbbe46642ac87c
Lex Talionis <lextalionis1488@gmail.com>
2022-04-27 11:13:54 +0100

    Object member This

---
+++ Обекты/ladder.js
@@ -0,0 +1,16 @@
+const ladder = {
+    step: 0,
+    up() {
+      this.step++;
+      return this;
+    },
+    down() {
+      this.step--;
+      return this;
+    },
+    showStep: function() { // показывает текущую ступеньку
+      console.log( this.step );
+    }
+  };
+
+  ladder.up().up().down().showStep();
