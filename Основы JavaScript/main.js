f01026a70ba98969aa9139002f532847e6d8aabe
Lex Talionis <lextalionis1488@gmail.com>
2022-04-27 07:05:28 +0100

    Conditions

--- Основы JavaScript/main.js
+++ Основы JavaScript/main.js
@@ -1,6 +1,12 @@
-const jsName = prompt('Какое «официальное» название JavaScript?', '')
-const result = (jsName === 'ECMAScript' 
-                ? 'Верно!' 
-                : 'Не знаете? ECMAScript!')
-alert(result)
+const input = prompt('Введите число', '')
+const num = Number(input)
+let result
 
+if (num > 0)
+    result = 1
+else if(num < 0)    
+    result = -1
+else if(num == 0)   
+    result = 0
+
+alert(result)    
